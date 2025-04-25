ec865c37b2cd9f33ea7139fab44c6f9d0180fb79
aaron moroney <aaron.createdesign@gmail.com>
2025-04-25 13:53:52 +0100

    #15: fix(engagement): Update pages

--- src/pages/leaderboard/ui/LeaderPage.tsx
+++ src/pages/leaderboard/ui/LeaderPage.tsx
@@ -1,47 +1,43 @@
-import * as React from  'react';
+import * as React from 'react';
 import { 
-    Box, 
-    Typography 
+	Box, 
+	Typography 
 } from '@mui/material';
 
 import { Leaderboard, ToggleTimePeriod } from 'features/leaderboard';
 import { useUsersActions, useUsersState } from 'entities/users/model';
 
 const styles = {
-    leader__page__container: {
-        marginTop: '30px',
-        height: '95%',
-        overflow: 'overlay',
-        '&::-webkit-scrollbar': {
-            width: '1px',
-        },
-        '&::-webkit-scrollbar-track': {
-            background: 'transparent', 
-        },
-    },
-}
+	leader__page__container: {
+		marginTop: '30px',
+		height: '95%',
+		overflow: 'overlay',
+		'&::-webkit-scrollbar': {
+			width: '1px',
+		},
+		'&::-webkit-scrollbar-track': {
+			background: 'transparent',
+		},
+	},
+};
 
 const LeaderPage = () => {
-    const { getUsers } = useUsersActions();
-    const { users } = useUsersState();
-    
-    React.useEffect(() => {
-        getUsers();
-    }, []);
+	const { getUsers } = useUsersActions();
+	const { users } = useUsersState();
 
-    return (
-        <>
-            <Typography variant='h4'>
-                Leaderboard
-            </Typography>
-            <Box sx={styles.leader__page__container}>
-                <ToggleTimePeriod />
-                <Leaderboard users={users} />
-            </Box>
-        </>
-    )
-}
+	React.useEffect(() => {
+		getUsers();
+	}, []);
 
-export default LeaderPage
+	return (
+		<>
+			<Typography variant='h4'>Leaderboard</Typography>
+			<Box sx={styles.leader__page__container}>
+				<ToggleTimePeriod />
+				<Leaderboard users={users} />
+			</Box>
+		</>
+	);
+};
 
-
+export default LeaderPage;
